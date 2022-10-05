753686888e1573cf2cc4a49bd1bb17c45bf33b1c
RogVan <roger.vanburgh@softwire.com>
2022-10-05 08:07:56 +0000

    FIXED: Debug launch config and integration tests

--- AgentDiscoveries-Backend/src/test/java/org/softwire/training/api/integration/DailyMessageIT.java
+++ AgentDiscoveries-Backend/src/test/java/org/softwire/training/api/integration/DailyMessageIT.java
@@ -1,58 +1,58 @@
-//package org.softwire.training.api.integration;
-//
-//import org.junit.jupiter.api.AfterAll;
-//import org.junit.jupiter.api.BeforeAll;
-//import org.junit.jupiter.api.Test;
-//import org.openqa.selenium.By;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.TimeoutException;
-//import org.openqa.selenium.WebElement;
-//import org.openqa.selenium.support.ui.ExpectedConditions;
-//import org.openqa.selenium.support.ui.WebDriverWait;
-//import org.softwire.training.api.core.MessageProcessor;
-//import org.softwire.training.api.integration.helper.LoginHelper;
-//import org.softwire.training.api.integration.helper.WebDriverHelper;
-//
-//import static org.junit.jupiter.api.Assertions.assertTrue;
-//
-//public class DailyMessageIT {
-//
-//    public static final String TARGET_ADDRESS = System.getProperty("target.address");
-//
-//    private static WebDriver driver;
-//    private static WebDriverWait wait;
-//
-//    @BeforeAll
-//    public static void setUp() {
-//        driver = WebDriverHelper.getSharedDriver();
-//        wait = new WebDriverWait(driver, 10);
-//    }
-//
-//    @Test
-//    public void testCanEncodeAndDecodeMessage() {
-//        MessageProcessor messageProcessor = new MessageProcessor("dev");
-//
-//        String plaintext = "aTestMessage";
-//        String encoded = messageProcessor.encode(plaintext);
-//
-//        driver.get(TARGET_ADDRESS);
-//        LoginHelper.ensureLoggedIn(driver);
-//        driver.get(TARGET_ADDRESS + "/#/message");
-//
-//        WebElement messageInput = driver.findElement(By.id("message-input"));
-//        messageInput.sendKeys(plaintext);
-//
-//        WebElement encodeButton = driver.findElement(By.id("encode-button"));
-//        encodeButton.click();
-//
-//        WebElement resultDiv = driver.findElement(By.id("code-result"));
-//        wait.until(ExpectedConditions.textToBePresentInElement(resultDiv, encoded));
-//
-//        messageInput.clear();
-//        messageInput.sendKeys(encoded);
-//
-//        WebElement decodeButton = driver.findElement(By.id("decode-button"));
-//        decodeButton.click();
-//        wait.until(ExpectedConditions.textToBePresentInElement(resultDiv, plaintext));
-//    }
-//}
+package org.softwire.training.api.integration;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.softwire.training.api.core.MessageProcessor;
+import org.softwire.training.api.integration.helper.LoginHelper;
+import org.softwire.training.api.integration.helper.WebDriverHelper;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DailyMessageIT {
+
+   public static final String TARGET_ADDRESS = System.getProperty("target.address");
+
+   private static WebDriver driver;
+   private static WebDriverWait wait;
+
+   @BeforeAll
+   public static void setUp() {
+       driver = WebDriverHelper.getSharedDriver();
+       wait = new WebDriverWait(driver, 10);
+   }
+
+   @Test
+   public void testCanEncodeAndDecodeMessage() {
+       MessageProcessor messageProcessor = new MessageProcessor("dev");
+
+       String plaintext = "aTestMessage";
+       String encoded = messageProcessor.encode(plaintext);
+
+       driver.get(TARGET_ADDRESS);
+       LoginHelper.ensureLoggedIn(driver);
+       driver.get(TARGET_ADDRESS + "/#/message");
+
+       WebElement messageInput = driver.findElement(By.id("message-input"));
+       messageInput.sendKeys(plaintext);
+
+       WebElement encodeButton = driver.findElement(By.id("encode-button"));
+       encodeButton.click();
+
+       WebElement resultDiv = driver.findElement(By.id("code-result"));
+       wait.until(ExpectedConditions.textToBePresentInElement(resultDiv, encoded));
+
+       messageInput.clear();
+       messageInput.sendKeys(encoded);
+
+       WebElement decodeButton = driver.findElement(By.id("decode-button"));
+       decodeButton.click();
+       wait.until(ExpectedConditions.textToBePresentInElement(resultDiv, plaintext));
+   }
+}
--- AgentDiscoveries-Backend/src/test/java/org/softwire/training/api/routes/v1/AgentsRoutesTest.java
+++ AgentDiscoveries-Backend/src/test/java/org/softwire/training/api/routes/v1/AgentsRoutesTest.java
@@ -1,90 +1,90 @@
-//package org.softwire.training.api.routes.v1;
-//
-//import org.junit.jupiter.api.Test;
-//import org.softwire.training.api.core.PermissionsVerifier;
-//import org.softwire.training.api.models.ErrorCode;
-//import org.softwire.training.api.models.FailedRequestException;
-//import org.softwire.training.api.testutils.RequestGenerationHelper;
-//import org.softwire.training.db.daos.AgentsDao;
-//import org.softwire.training.db.daos.UsersDao;
-//import org.softwire.training.models.Agent;
-//import org.softwire.training.models.User;
-//import spark.Request;
-//import spark.Response;
-//
-//import java.time.LocalDate;
-//import java.util.Optional;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.assertThrows;
-//import static org.mockito.Matchers.any;
-//import static org.mockito.Mockito.mock;
-//import static org.mockito.Mockito.verify;
-//import static org.mockito.Mockito.when;
-//
-//public class AgentsRoutesTest {
-//
-//    private AgentsDao agentsDao = mock(AgentsDao.class);
-//    private UsersDao usersDao = mock(UsersDao.class);
-//    private PermissionsVerifier permissionsVerifier = new PermissionsVerifier(usersDao);
-//    private AgentsRoutes agentsRoutes = new AgentsRoutes(agentsDao, permissionsVerifier);
-//    private Response response = mock(Response.class);
-//
-//    @Test
-//    public void readAgentFailsIfAgentDoesNotExist() {
-//        // Given
-//        int agentId = 17;
-//        Agent agent = new Agent();
-//        agent.setAgentId(agentId);
-//        agent.setFirstName("bob");
-//        agent.setDateOfBirth(LocalDate.now());
-//
-//        int userId = 15;
-//        User admin = new User();
-//        admin.setUserId(userId);
-//        admin.setAgentId(agentId);
-//        admin.setAdmin(true);
-//
-//        Request request = RequestGenerationHelper.makeRequestWithJSONBodyForObject(agent);
-//
-//        when(agentsDao.getAgent(agentId)).thenReturn(Optional.empty());
-//        when(request.attribute("user_id")).thenReturn(0);
-//        when(usersDao.getUser(0)).thenReturn(Optional.of(admin));
-//
-//        // When
-//        FailedRequestException exception = assertThrows(
-//                FailedRequestException.class,
-//                () -> agentsRoutes.readAgent(request, response, agentId));
-//
-//        // Then
-//        assertEquals(ErrorCode.NOT_FOUND, exception.getErrorCode());
-//    }
-//
-//    @Test
-//    public void readAgentReturnsAgentIfAgentExists() {
-//        // Given
-//        int agentId = 17;
-//        Agent agent = new Agent();
-//        agent.setAgentId(agentId);
-//        agent.setFirstName("bob");
-//        agent.setDateOfBirth(LocalDate.now());
-//
-//        int userId = 15;
-//        User admin = new User();
-//        admin.setUserId(userId);
-//        admin.setAgentId(agentId);
-//        admin.setAdmin(true);
-//
-//        Request request = RequestGenerationHelper.makeRequestWithJSONBodyForObject(agent);
-//
-//        when(agentsDao.getAgent(agentId)).thenReturn(Optional.of(agent));
-//        when(request.attribute("user_id")).thenReturn(0);
-//        when(usersDao.getUser(0)).thenReturn(Optional.of(admin));
-//
-//        // When
-//        Agent returnedAgent = agentsRoutes.readAgent(request, response, agentId);
-//
-//        // Then
-//        assertEquals(agent, returnedAgent);
-//    }
-//}
+package org.softwire.training.api.routes.v1;
+
+import org.junit.jupiter.api.Test;
+import org.softwire.training.api.core.PermissionsVerifier;
+import org.softwire.training.api.models.ErrorCode;
+import org.softwire.training.api.models.FailedRequestException;
+import org.softwire.training.api.testutils.RequestGenerationHelper;
+import org.softwire.training.db.daos.AgentsDao;
+import org.softwire.training.db.daos.UsersDao;
+import org.softwire.training.models.Agent;
+import org.softwire.training.models.User;
+import spark.Request;
+import spark.Response;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class AgentsRoutesTest {
+
+   private AgentsDao agentsDao = mock(AgentsDao.class);
+   private UsersDao usersDao = mock(UsersDao.class);
+   private PermissionsVerifier permissionsVerifier = new PermissionsVerifier(usersDao);
+   private AgentsRoutes agentsRoutes = new AgentsRoutes(agentsDao, permissionsVerifier);
+   private Response response = mock(Response.class);
+
+   @Test
+   public void readAgentFailsIfAgentDoesNotExist() {
+       // Given
+       int agentId = 17;
+       Agent agent = new Agent();
+       agent.setAgentId(agentId);
+       agent.setFirstName("bob");
+       agent.setDateOfBirth(LocalDate.now());
+
+       int userId = 15;
+       User admin = new User();
+       admin.setUserId(userId);
+       admin.setAgentId(agentId);
+       admin.setAdmin(true);
+
+       Request request = RequestGenerationHelper.makeRequestWithJSONBodyForObject(agent);
+
+       when(agentsDao.getAgent(agentId)).thenReturn(Optional.empty());
+       when(request.attribute("user_id")).thenReturn(0);
+       when(usersDao.getUser(0)).thenReturn(Optional.of(admin));
+
+       // When
+       FailedRequestException exception = assertThrows(
+               FailedRequestException.class,
+               () -> agentsRoutes.readAgent(request, response, agentId));
+
+       // Then
+       assertEquals(ErrorCode.NOT_FOUND, exception.getErrorCode());
+   }
+
+   @Test
+   public void readAgentReturnsAgentIfAgentExists() {
+       // Given
+       int agentId = 17;
+       Agent agent = new Agent();
+       agent.setAgentId(agentId);
+       agent.setFirstName("bob");
+       agent.setDateOfBirth(LocalDate.now());
+
+       int userId = 15;
+       User admin = new User();
+       admin.setUserId(userId);
+       admin.setAgentId(agentId);
+       admin.setAdmin(true);
+
+       Request request = RequestGenerationHelper.makeRequestWithJSONBodyForObject(agent);
+
+       when(agentsDao.getAgent(agentId)).thenReturn(Optional.of(agent));
+       when(request.attribute("user_id")).thenReturn(0);
+       when(usersDao.getUser(0)).thenReturn(Optional.of(admin));
+
+       // When
+       Agent returnedAgent = agentsRoutes.readAgent(request, response, agentId);
+
+       // Then
+       assertEquals(agent, returnedAgent);
+   }
+}
