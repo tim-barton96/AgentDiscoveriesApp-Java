0f0efe1afa36e110491bec3dcea4ccb2bf9b3ca0
tim-barton96 <tim.barton96@hotmail.com>
2022-10-11 15:32:15 +0000

    search results split neatly but no title

--- AgentDiscoveries-Frontend/app/src/components/search-forms/search-result.jsx
+++ AgentDiscoveries-Frontend/app/src/components/search-forms/search-result.jsx
@@ -12,9 +12,9 @@ export default class SearchResult extends React.Component {
     }
 
     renderResults(results) {
-        return results.map((result, index) => {
+        return results.map((result, reportId) => {
             return (
-                <Panel key={index}>
+                <Panel key={reportId}>
                     <Panel.Heading>Result</Panel.Heading>
                     <Panel.Body>{this.renderResultBody(result)}</Panel.Body>
                 </Panel>
@@ -22,10 +22,24 @@ export default class SearchResult extends React.Component {
         });
     }
 
+    // renderResultBody(result) {
+    //     return Object.keys(result).map(key => {
+    //         return <p key={key} id={key}>{`${key}: ${result[key]}`}</p>;
+    //     });
+    // }
+
     renderResultBody(result) {
-        return Object.keys(result).map(key => {
-            return <p key={key} id={key}>{`${key}: ${result[key]}`}</p>;
-        });
+        return (
+            <React.Fragment>
+                <div className='meta-info-container'>
+                    <p>Title: {result.reportTitle} </p>
+                    <p>Agent: {result.agentId}</p>
+                    <p>Status: {result.status}</p>
+                    <p className='time'>Date: {result.reportTime}</p>
+                </div>
+                <p>{result.reportBody}</p>
+            </React.Fragment>
+        );
     }
 
     getResultsHeader(results) {
